49bfc5f2755e39d9e4b379691ac942e359091fc7
lysy352 <dawid.siwko@gmail.com>
2019-05-29 23:49:28 +0200

    add domain to photo url

--- src/main/java/pl/edu/agh/places/external/google/factory/PhotoFactory.java
+++ src/main/java/pl/edu/agh/places/external/google/factory/PhotoFactory.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.places.external.google.factory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.edu.agh.places.domain.dto.Photo;
 import pl.edu.agh.places.external.google.api.details.response.DetailsPhoto;
@@ -7,14 +8,20 @@ import pl.edu.agh.places.external.google.api.details.response.DetailsPhoto;
 @Component
 public class PhotoFactory {
 
-    private static final String GOOGLE_PHOTO_PATH = "/v1/photos/google/%s";
+    private static final String GOOGLE_PHOTO_PATH = "/v1/photos/google/";
+
+    private final String domain;
+
+    public PhotoFactory(@Value("${app.domain}") String domain) {
+        this.domain = domain;
+    }
 
     public Photo create(DetailsPhoto detailsPhoto) {
         return new Photo(getUrl(detailsPhoto.getPhotoReference()));
     }
 
     private String getUrl(String photoReference) {
-        return String.format(GOOGLE_PHOTO_PATH, photoReference);
+        return String.format("%s%s%s", domain, GOOGLE_PHOTO_PATH, photoReference);
     }
 
 }
